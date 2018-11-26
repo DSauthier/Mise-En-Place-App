a10ac3eee0a4b15dd96a24c45b0d26f0eff9dfab
DSauthier <danielsauthier@live.com>
2018-11-26 18:30:57 -0500

    Adjusting style

---
+++ models/RecipeModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+// create a new schema object
+const Schema = mongoose.Schema;
+
+
+
+// you only need to do {type: string} if you are adding more rules like a default or minlength
+const RecipeSchema = new Schema({
+  name: String,
+  ingredients: [String],
+  directions: String,
+  time: Number,
+  image: String,
+  imgName: String,
+  cuisine:{ type: String, enum: ['Carnivore','Vegetarian','Vegan',"Seafood","Poultry","All Purpose","Basics"] },
+  author: { type: Schema.Types.ObjectId, ref: 'User' }
+},
+   {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+//3.1 you create the cat class using those rules
+const Recipe = mongoose.model('Recipe', RecipeSchema);
+
+
+module.exports = Recipe;
---
+++ routes/recipeRoute.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/RecipeModel');
+const uploadCloud = require("../config/cloudinary.js");
+
+// index recipe page-> show all recipes --==--=-=-=-=
+router.get('/', (req, res, next) => {
+    Recipe.find()
+      .then((allTheRecipes) => {
+        res.render('RecipesFolder/recipeIndex', { Recipes: allTheRecipes })
+      })
+      .catch((err) => {
+        next(err);
+      })
+  // }
+});
+// -=-=-=-=-=-==--=-==-show all recipes end-==--=-=-=-=-=
+
+// =-=--=-==-=-Create recipe -=--==--=-==--==-
+router.get('/new', (req, res, next) => {
+  if (!req.user) {
+    req.flash('error', 'sorry you must be logged in to create a recipe')
+    res.redirect('/login');
+  } else {
+    Recipe.find()
+      .then((allTheRecipes) => {
+        res.render('RecipesFolder/newRecipe', { allTheRecipes })
+      })
+      .catch((err) => {
+        next(err);
+      })
+  }
+});
+
+router.post('/recipes/create',uploadCloud.single('image'), (req, res, next) => {
+
+  const newRecipe = req.body;
+  newRecipe.author = req.user._id;
+  const image = req.file.url;
+  newRecipe.image = image
+  // const newMovie = new Movie({ title, description, imgPath, imgName })
+  Recipe.create(newRecipe)
+    .then(() => {
+      res.redirect('/recipes');
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+
+// =--=-=-=-=-=Create recipe ends -=-=--==--=-==-
+
+// =--=-=-==-=-=--=EDIT recipe starts=--=-=-=-=-==-=-
+
+
+router.get('/:_id/edit', (req, res, next) => {
+  Recipe.findById(req.params._id)
+    .then((theRecipe) => {
+      res.render('RecipesFolder/editRecipe', {theRecipe: theRecipe})
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
+
+router.post('/:_id/update', uploadCloud.single('image'), (req, res, next) => {
+  const updateAll = req.body;
+  const image = req.file.url;
+  updateAll.image = image;
+  Recipe.findByIdAndUpdate(req.params._id, updateAll)
+  
+    .then(() => {
+      // console.log("-=-==--=-=-=-==-"+theRecipe)
+      res.redirect("/recipes/" + req.params._id);
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+// =--=-=-=-=-=-=-=EDIT RECIPE ENDS=--==--=-=-=-=-=-=
+
+// =-=--=-=-=-=show each recipe detail starts-==--=-=-=
+router.get('/:id', (req, res, next) => {
+  let canDelete = false;
+  let canEdit = false;
+  Recipe.findById(req.params.id).populate('author')
+    .then((theRecipe) => {
+      if(req.user) {
+        // console.log("--------- ", theRecipe.author._id);
+        // console.log("=========", req.user._id);
+        if(String(theRecipe.author._id) == String(req.user._id)){
+          canDelete = true;
+        }
+      }
+
+      const theIngredients = theRecipe.ingredients[0].split("\n");
+      theIngredients.shift();
+
+      const theDirection = theRecipe.directions.split("\n");
+      theDirection.shift();
+      
+
+
+      data = { theRecipe: theRecipe, canDelete: canDelete, canEdit: canEdit, theIngredients, theDirection };
+      // console.log(data)
+      res.render('RecipesFolder/recipeDetails', data)
+  
+    })
+    .catch((err) => {
+      next(err);
+    })
+})
+// =--=-=-=-=-=show each recipe detail ends-=-=-=-=-=-=
+
+// -==--=-=-=delete Recipe starts=--=-=-=-=-=
+router.post('/:id/delete', (req, res, next) => {
+  Recipe.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.redirect('/recipes')
+    })
+    .catch((err) => {
+      console.log("error")
+      next(err);
+    })
+})
+// =-=--=-=-=-=delete recipe ends=--=-=-=-=-=
+module.exports = router;
